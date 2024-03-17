455a656400133dadf13d8d7fb74b99addc7323bc
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2024-03-17 16:36:41 +0530

    Modified recommendations.html

--- Code/app/views.py
+++ Code/app/views.py
@@ -1,13 +1,10 @@
 # -*- encoding: utf-8 -*-
-
-
 # Python modules
 import os, logging 
 import pandas as pd
 
-
 # Flask modules
-from flask               import render_template, request, url_for, redirect, send_from_directory
+from flask               import make_response, render_template, request, url_for, redirect, send_from_directory
 from flask_login         import login_user, logout_user, current_user, login_required
 from werkzeug.exceptions import HTTPException, NotFound, abort
 from jinja2              import TemplateNotFound
@@ -32,73 +29,54 @@ def logout():
 
 # Register a new user
 @app.route('/register', methods=['GET', 'POST'])
-def register():
-    
+def register():    
     # declare the Registration Form
     form = RegisterForm(request.form)
-
     msg     = None
     success = False
 
     if request.method == 'GET': 
-
         return render_template( 'register.html', form=form, msg=msg )
 
     # check if both http method is POST and form is valid on submit
     if form.validate_on_submit():
-
         # assign form data to variables
         username = request.form.get('username', '', type=str)
         password = request.form.get('password', '', type=str) 
         email    = request.form.get('email'   , '', type=str) 
-
         # filter User out of database through username
         user = Users.query.filter_by(user=username).first()
-
         # filter User out of database through username
         user_by_email = Users.query.filter_by(email=email).first()
 
         if user or user_by_email:
-            msg = 'Error: User exists!'
-        
+            msg = 'Error: User exists!'   
         else:         
-
             pw_hash = bc.generate_password_hash(password)
-
             user = Users(username, email, pw_hash)
-
             user.save()
-
             msg     = 'User created, please <a href="' + url_for('login') + '">login</a>'     
             success = True
-
     else:
         msg = 'Input error'     
-
     return render_template( 'register.html', form=form, msg=msg, success=success )
 
 # Authenticate user
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    
     # Declare the login form
     form = LoginForm(request.form)
-
     # Flask message injected into the page, in case of any errors
     msg = None
-
     # check if both http method is POST and form is valid on submit
     if form.validate_on_submit():
-
         # assign form data to variables
         username = request.form.get('username', '', type=str)
         password = request.form.get('password', '', type=str) 
-
         # filter User out of database through username
         user = Users.query.filter_by(user=username).first()
 
         if user:
-            
             if bc.check_password_hash(user.password, password):
                 login_user(user)
                 return redirect(url_for('calci'))
@@ -106,40 +84,29 @@ def login():
                 msg = "Wrong password. Please try again."
         else:
             msg = "Unknown user"
-
     return render_template( 'login.html', form=form, msg=msg )
 
 # App main route + generic routing
 @app.route('/', defaults={'path': 'index'})
 @app.route('/<path>')
 def index(path):
-
     #if not current_user.is_authenticated:
     #    return redirect(url_for('login'))
-
     try:
-
         return render_template( 'index.html' )
-    
     except TemplateNotFound:
         return render_template('page-404.html'), 404
-    
     except:
         return render_template('page-500.html'), 500
 
 @app.route('/details')
 def details():
-
     #if not current_user.is_authenticated:
     #    return redirect(url_for('login'))
-
     try:
-
         return render_template( 'details.html' )
-    
     except TemplateNotFound:
         return render_template('page-404.html'), 404
-    
     except:
         return render_template('page-500.html'), 500
     
@@ -184,10 +151,13 @@ def recommendations():
         community_column = 'ST'
 
     # Filter the dataset based on user inputs
-    filtered_data = dataset[(dataset[community_column] >= float(cutoff)) &
-                           (dataset['Branch_Name'] == 'Civil Engineering')]
+    filtered_data = dataset[(float(cutoff) >= dataset[community_column]) &
+                           (dataset['Branch_Code'] == branch) &
+                           (location.lower() in dataset['College_Name'].str.lower())]
+    
+    print(filtered_data)
 
- # Drop duplicate rows based on College_Name
+    # Drop duplicate rows based on College_Name
     filtered_data.drop_duplicates(subset=['College_Name'], keep='first', inplace=True)
 
     # Select only the specified columns
@@ -198,3 +168,23 @@ def recommendations():
     table_html = filtered_data.to_html(index=False, classes='table table-striped')
 
     return render_template('recommendations.html', table=table_html)
+
+@app.route('/export_excel', methods=['POST'])
+def export_excel():
+    # Get the HTML table data from the POST request
+    html_table = request.form['html_table']
+
+    # Convert HTML table to pandas DataFrame
+    df = pd.read_html(html_table)[0]  # Assuming there's only one table
+
+    # Export DataFrame to Excel
+    excel_data = df.to_excel(index=False)
+
+    # Create response object
+    response = make_response(excel_data)
+
+    # Set headers for file download
+    response.headers['Content-Disposition'] = 'attachment; filename=table_data.xlsx'
+    response.headers['Content-Type'] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
+    return response
--- Code/run.py
+++ Code/run.py
@@ -2,3 +2,9 @@
 
 
 from app import app, db
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
